d3a2045fa3ced06c8d9c04a98027c39e3540e4d4
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-03 21:12:04 +0000

    Created Provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/* Braces mean we want to import a named export 
+from the file rather than a default export */
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import reducers from './reducers';
 
 ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
+  /* Create an instance of the provider around the App component.
+     -- Pass the Provider a single prop called store.
+     -- The store is going to be the result of calling createStore
+        -- and pasing in our reducers */
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );

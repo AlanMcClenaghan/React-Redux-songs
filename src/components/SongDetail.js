0b8dd27886d5f38a6c76a2176959aa280873363a
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-03-04 12:38:28 +0000

    SongDetail component.

---
+++ src/components/SongDetail.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SongDetail = ({ selectedSong }) => {
+  console.log(selectedSong)
+
+  if (!selectedSong) {
+    return <h2>Select a song</h2>
+  }
+
+  return (
+    <div>
+      <h2>Song Detail</h2>
+      <p>Title: {selectedSong.title}</p>
+      <p>Duration: {selectedSong.duration}</p>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return { selectedSong: state.selectedSong }
+}
+
+export default connect(mapStateToProps)(SongDetail);
